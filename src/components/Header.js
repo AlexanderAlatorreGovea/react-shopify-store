b22bb0187320ce9bf11a4a0ebfc9a230cdd3e25b
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-08-18 16:18:05 -0600

    First commit.

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import "./Header";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+          <img/>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
