6c78ed875e28ae6f12c8b8f8cc9032367efc02b1
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-08-19 20:45:14 -0600

    Started adding redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Client from 'shopify-buy';
-import reportWebVitals from "./reportWebVitals"; 
-import './index.scss';
+import Client from "shopify-buy";
+import reportWebVitals from "./reportWebVitals";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./index.scss";
+
+const store = createStore(rootReducer);
 
 const client = Client.buildClient({
   storefrontAccessToken: process.env.REACT_APP_API_KEY,
-  domain: process.env.REACT_APP_BASE_URL
+  domain: process.env.REACT_APP_BASE_URL,
 });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App client={client} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
